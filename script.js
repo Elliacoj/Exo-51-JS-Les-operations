28ca763b2912e3e78e471770a9baac95812bd908
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-23 13:38:01 +0100

    Modif

---
+++ script.js
@@ -0,0 +1,12 @@
+let result;
+result = 5 + 6;
+
+let addition;
+addition = result + 2;
+
+let soustraction;
+soustraction = result - 1;
+
+alert (result * addition)
+alert (result ** soustraction)
+alert (5%2)
